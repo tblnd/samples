b9ecab31c30848c0f9084489b5b0d765f0c47b91
tblnd <tommy_beland@hotmail.com>
2021-10-26 14:52:04 -0400

    golang packages

---
+++ golang/ns-pid.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+	exec.Command("/bin/bash")
+	cmd := exec.Cmd{
+		Path:   "/bin/bash",
+		Stdin:  os.Stdin,
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+		SysProcAttr: &syscall.SysProcAttr{
+			Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+		},
+	}
+	if err := cmd.Run(); err != nil {
+		fmt.Println(err)
+	}
+}
